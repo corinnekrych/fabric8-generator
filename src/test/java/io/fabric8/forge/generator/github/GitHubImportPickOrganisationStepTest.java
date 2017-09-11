2f958e2aadb3b5c75487b872e59396de57e07bf1
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-09-11 11:54:03 +0200

    fix: test infrastructure

--- src/test/java/io/fabric8/forge/generator/github/GitHubImportPickOrganisationStepTest.java
+++ src/test/java/io/fabric8/forge/generator/github/GitHubImportPickOrganisationStepTest.java
@@ -1,21 +1,27 @@
 package io.fabric8.forge.generator.github;
 
-import org.jboss.arquillian.container.test.api.Deployment;
+import javax.inject.Inject;
 import org.jboss.arquillian.junit.Arquillian;
-//import org.jboss.forge.arquillian.archive.ForgeArchive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.addon.shell.test.ShellTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+//import org.jboss.forge.arquillian.archive.ForgeArchive;
+
 @RunWith(Arquillian.class)
 public class GitHubImportPickOrganisationStepTest {
 
+    @Inject
+    private ShellTest shellTest;
+
+    @Inject
+    protected ProjectFactory projectFactory;
 
     @Test
     public void testSomething() throws Exception {
-        Assert.assertEquals("Not implemented", "Not implemented");
+        Assert.assertNotNull(shellTest);
+        Assert.assertNotNull(projectFactory);
     }
 }
